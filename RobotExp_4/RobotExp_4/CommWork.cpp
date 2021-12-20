467504f165b416e36e8531b8199989731912a6c5
msjun23 <msjun23@gmail.com>
2021-12-20 11:15:14 +0900

    Project complete

---
+++ RobotExp_4/RobotExp_4/CommWork.cpp
@@ -0,0 +1,119 @@
+#include "stdafx.h"
+#include "DataType.h"
+#include "CommWork.h"
+#include "SystemMemory.h"
+
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+
+CCommWork::CCommWork(std::string name)
+	:CWorkBase(name)
+{
+	memset(&_target, 0, sizeof(ControlData_t));
+	memset(&_current, 0, sizeof(ControlData_t));
+
+	_memname_tar = name + "_Controller_Target";
+	_memname_cur = name + "_Controller_Current";
+	CREATE_SYSTEM_MEMORY(_memname_tar, ControlData_t);
+	CREATE_SYSTEM_MEMORY(_memname_cur, ControlData_t);
+
+	_sendPacket.data.header[0] = _sendPacket.data.header[1] = _sendPacket.data.header[2] = _sendPacket.data.header[3] = 0xFF;
+	_sendPacket.data.id = 1;
+	_sendPacket.data.mode = 2;
+	_sendPacket.data.size = sizeof(Packet_t);
+
+}
+
+
+CCommWork::~CCommWork() {
+
+	DELETE_SYSTEM_MEMORY(_memname_tar);
+	DELETE_SYSTEM_MEMORY(_memname_cur);
+
+	ClosePort();
+}
+
+
+
+bool CCommWork::OpenPort(std::string name, int baudRate) {
+
+	return _comm.Open(name.c_str(), baudRate);
+}
+
+
+
+void CCommWork::ClosePort() {
+
+	_comm.Close();
+}
+
+
+
+void CCommWork::_execute() {
+	//_memname_tar = "Comm1Work_Controller_Target"
+	GET_SYSTEM_MEMORY(_memname_tar, _target);
+
+	static int mode, readSize = 0, checkSize;
+	static unsigned char check;
+
+	if (_comm.isOpen()) {
+		_sendPacket.data.check = 0;
+		_sendPacket.data.pos = _target.position * 1000;
+		_sendPacket.data.velo = _target.velocity * 1000;
+		_sendPacket.data.cur = _target.current * 1000;
+
+		for (int i = 8; i < sizeof(Packet_t); i++) {
+			_sendPacket.data.check += _sendPacket.buffer[i];
+		}
+		_comm.Write((char*)_sendPacket.buffer, sizeof(Packet_t));
+
+		readSize = _comm.Read((char*)_recvBuf, 4096);
+		//printf("Read : %d\n", readSize);
+
+		for (int i = 0; i < readSize; i++) {
+			switch (mode) {
+			case 0:
+				if (_recvBuf[i] == 0xFE) {
+					checkSize++;
+					if (checkSize == 4) {
+						mode = 1;
+					}
+				}
+				else {
+					checkSize = 0;
+				}
+				break;
+			case 1:
+				_packet.buffer[checkSize++] = _recvBuf[i];
+
+				if (checkSize == 8) {
+					mode = 2;
+				}
+				break;
+			case 2:
+				_packet.buffer[checkSize++] = _recvBuf[i];
+				check += _recvBuf[i];
+
+				if (checkSize == _packet.data.size) {
+					if (check == _packet.data.check) {
+						/*printf("pos : %f, velo : %f, cur : %f\n",
+							_packet.data.pos / 1000 * RAD2DEG,
+							_packet.data.velo / 1000 * RAD2DEG,
+							_packet.data.cur / 1000 * 0.0683);*/
+
+						_current.position = _packet.data.pos / 1000.;
+						_current.velocity = _packet.data.velo / 1000.;
+						_current.current = _packet.data.cur / 1000.;
+					}
+					//memset(_recvBuf, 0, readSize);
+
+					check = 0;
+					mode = 0;
+					checkSize = 0;
+				}
+			}
+		}
+	}
+
+	// Comm1Work_Controller_Current
+	SET_SYSTEM_MEMORY(_memname_cur, _current);
+}
